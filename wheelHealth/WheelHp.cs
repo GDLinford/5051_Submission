49bf54dfafadf0101f9d8999edfc434a25fc81b3
GDLinford <55755896+GDLinford@users.noreply.github.com>
2021-04-09 21:24:59 +0100

    more

---
+++ wheelHealth/WheelHp.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace wheelHealth
+{
+    public class WheelHp : MonoBehaviour
+    {
+        private int maxHealth;
+        public int WheelsHP;
+
+        public void SetHealth(int amount)
+        {
+            WheelsHP = Mathf.Clamp(WheelsHP, 0, 50);
+
+            WheelsHP = maxHealth = amount;
+        }
+
+        public IEnumerator DelayDamage(int amount, int delay)
+        {
+            yield return new WaitForSeconds(delay);
+            //never go below 0
+            WheelsHP = Mathf.Max(WheelsHP - amount, 0);
+        }
+
+    }
+}
+
+
